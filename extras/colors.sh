fed2d4855be041b4b4d728da127b7e1c5d32f24b
jnbnyc <jnbuhaynyc@gmail.com>
2018-11-04 17:38:11 -0500

    colors

---
+++ extras/colors.sh
@@ -0,0 +1,17 @@
+# functions for colors
+
+function red {
+  echo -e "\033[00;31m$1\033[0m"
+}
+
+function green {
+  echo -e "\033[00;32m$1\033[0m"
+}
+
+function yellow {
+  echo -e "\033[00;33m$1\033[0m"
+}
+
+function blue {
+  echo -e "\033[00;34m$1\033[0m"
+}
