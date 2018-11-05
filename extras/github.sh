ef02fb9fffdcd35bd94c08a8803cc775546e1154
jnbnyc <jnbuhaynyc@gmail.com>
2018-11-04 20:24:23 -0500

    github

--- extras/github.sh
+++ extras/github.sh
@@ -1,16 +1,38 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 # https://developer.github.com/v3/orgs/teams/
 
 GITHUB_TOKEN=$(cat ~/.github-token)
 
-function github-clone { git clone https://github.com/$1.git $2; }
-alias ghcl='github-clone'
+alias ghd='cd $WORKSPACE_GITHUB'
+
+# function clone https://github.com/org|user/repo.git
+function clone {
+    __DIR=$WORKSPACE/$(echo $1 | sed -E "s@(https://|.git)@@g" | xargs dirname)
+    if [ -d "$__DIR" ]; then
+      return 1
+    else
+      mkdir $__DIR
+    fi
+    cd $__DIR
+    git clone $1
+    cd -
+}
+
+function github-clone {
+    git clone https://github.com/$1.git $2;
+}
 
 # functions for jnbnyc-github
-function jnbnyc-clone  { git clone https://github.com/jnbnyc/$1.git; }  #  ssh config specifies which key to use
-function jnbnyc-create { curl -X POST -u jnbnyc -d "{\"name\": \"$1\" }" https://api.github.com/user/repos; }
+function jnbnyc-clone { git clone https://github.com/jnbnyc/$1.git; }
+
+function jnbnyc-create {
+    curl -X POST -u jnbnyc \
+        -d "{\"name\": \"$1\" }" https://api.github.com/user/repos
+}
 
 
 function get_github_team_id() {
     curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/orgs/$GITHUB_ORG/teams
 }
+
+echo 'Loaded github shortcuts'
